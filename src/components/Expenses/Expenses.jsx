14581b4392ab6ace15fc03ee1d922ac2ada47401
Roman Dubovik <ip.romandubovik@gmail.com>
2023-05-09 21:41:44 +0300

    favicons added

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -6,7 +6,7 @@ import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = ({ items }) => {
-  const [filteredYear, setFilteredYear] = useState('2021');
+  const [filteredYear, setFilteredYear] = useState('2023');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
